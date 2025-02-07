8b2bfd7bf3dab036d521a15028e9a2b71b048177
BothSann <boththann76@gmail.com>
2025-02-07 19:07:17 +0700

    add functionality to fetch quotes and tweet selected quotes

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 "use strict";
+
+// Select DOM elements
 const quoteContent = document.querySelector(".quote__content");
 const quoteAuthor = document.querySelector(".quote__author--name");
+const tweetBtn = document.querySelector(".quote__button--twitter");
+const newQuoteBtn = document.querySelector(".quote__button--new");
+
+// Variable to store API quotess
 let apiQuotes = [];
 
+// Function to fetch quotes
+const getQuotes = async function () {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+// Function to display a new quote
 const newQuote = function () {
+  if (apiQuotes.length === 0) return;
+
   const randomQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   // Handle author
@@ -28,15 +49,15 @@ const newQuote = function () {
   }
 };
 
-const getQuotes = async function () {
-  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (err) {
-    alert(err.message);
-  }
+// Function to tweet the quote
+const tweetQuote = function () {
+  const twitterURL = `https://twitter.com/intent/tweet?text=${quoteContent.textContent} - ${quoteAuthor.textContent}`;
+  window.open(twitterURL, "_blank");
 };
 
+// Event Listeners
+tweetBtn.addEventListener("click", tweetQuote);
+newQuoteBtn.addEventListener("click", newQuote);
+
+// On load, fetch quotes
 getQuotes();
